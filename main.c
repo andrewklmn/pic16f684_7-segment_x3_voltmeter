6f0b2621587983f764f6b0fa05f0576510fc0ab8
andrewklmn <user@localhost>
2018-04-02 16:24:14 +0300

    project start

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 #pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
 #pragma config BOREN = OFF      // Brown Out Detect (BOR disabled)
 #pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode is disabled)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config FCMEN = OFF      //  Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
 
 #define INPUT RA2   //ADC input 
 
